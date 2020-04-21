2e84e41176a0ab17a720508c01c0d42942061cc2
Daniel Souza Amazonas <daniel.amazonas@gmail.com>
2020-04-21 12:56:46 -0300

    Adicionado a randomização da comida

--- js/main.js
+++ js/main.js
@@ -24,8 +24,8 @@ function criarSnake() {
     }
 }
 
-const img = new Image()
-img.src = './img/virus.png'
+let img = new Image()
+img.src = './img/virus' + Math.floor(Math.random() * 8) + '.png'
 
 function drawFood() {
     context.fillStyle = context.createPattern(img, 'repeat')
@@ -68,10 +68,11 @@ function iniciarGame() {
     if(direction == 'down') snakeY += box
 
     if(snakeX != food.x || snakeY != food.y) {
-        snake.pop()    
+        snake.pop()
     } else {
         food.x = Math.floor(Math.random() * 15 + 1) * box
         food.y = Math.floor(Math.random() * 15 + 1) * box
+        img.src = './img/virus' + Math.floor(Math.random() * 8) + '.png'
     }
 
     let newHead = {
